5df2c21f9a10e3ca1bc263a843298368533ba5dc
Aetopia <kirbymario22@gmail.com>
2023-07-11 23:36:18 +0530

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 @echo off
 cd "%~dp0"
-g++ -mwindows -s -Os WinMain.cpp -lole32 -o WinVolumeHotKeys.exe
+g++ -mwindows -s -Os WinMain.cpp -lole32 -lwinmm -o WinVolumeHotKeys.exe
 upx --best --ultra-brute WinVolumeHotKeys.exe>nul 2>&1
